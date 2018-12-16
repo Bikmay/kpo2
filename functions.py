4192ecf1ae43b213ba0ebae4d2cc6067c0779b78
Bikmay <artyom.ivliev@gmail.com>
2018-12-17 02:30:34 +0400

    push

--- functions.py
+++ functions.py
@@ -1,11 +1,16 @@
 import numpy as np
 
+
+
+from decimal import Decimal, ROUND_FLOOR
+
+
 def comprasion_k(k,mass):
 
     sum1=0
 
     for i in range(7):
-        sum1+=np.e**k*(i+1)
+        sum1+=np.e**-2*k*(i+1)
 
 
     sum2=0
@@ -21,6 +26,10 @@ def comprasion_k(k,mass):
     for i in range(7):
         sum4+=mass[i]*np.e**(-1*k*(i+1))
 
+    d_sum1=Decimal(sum1*sum2/sum3)
+    print(Decimal(sum1*sum2/sum3).quantize(Decimal("1.000"), ROUND_FLOOR) - Decimal(sum4).quantize(Decimal("1.000"), ROUND_FLOOR))
+    print(Decimal(sum1*sum2/sum3).quantize(Decimal("1.000"), ROUND_FLOOR))
+    print(Decimal(sum4).quantize(Decimal("1.000"), ROUND_FLOOR))
+    print('\n')
 
-    print(round(sum1*sum2/sum3,5) - round(sum4,5))
-    return round(sum1*sum2/sum3,5) == round(sum4,5)
+    return Decimal(sum1*sum2/sum3).quantize(Decimal("1.000"), ROUND_FLOOR) == Decimal(sum4).quantize(Decimal("1.000"), ROUND_FLOOR)
--- main.py
+++ main.py
@@ -11,13 +11,13 @@ input_mass=(input().split(' '));
 for i in range(7):
     input_mass[i]=int(input_mass[i])
 
-K = 0.01
+K = 0.001
 check = f.comprasion_k(K, input_mass)
 while not check:
-    K += 0.01
+    K += 0.001
     print(K)
     check = f.comprasion_k(K, input_mass)
-    if(K>0.99):
+    if(K>0.999):
         break
 
 n=fn.findN(K,input_mass)
