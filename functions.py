80c39189dd010a3abccb762a71f4f94d2bd9c59d
se_ga <jopa123JOPA>
2018-12-17 01:51:45 +0400

    kuku

--- functions.py
+++ functions.py
@@ -5,21 +5,21 @@ def comprasion_k(k,mass):
     sum1=0
 
     for i in range(7):
-        sum1+=np.e**k*i
+        sum1+=np.e**k*(i+1)
 
 
     sum2=0
     for i in range(7):
-        sum2+=mass[i]*np.e**(k*-1.0*i)
+        sum2+=mass[i]*np.e**(k*-1.0*(i+1))
 
     sum3=0
     for i in range(7):
-        sum3+=np.e**(-2*k*i)
+        sum3+=np.e**(-2*k*(i+1))
 
     sum4=0
 
     for i in range(7):
-        sum4+=mass[i]*np.e**(-1*k*i)
+        sum4+=mass[i]*np.e**(-1*k*(i+1))
 
 
     print(round(sum1*sum2/sum3,5) - round(sum4,5))
